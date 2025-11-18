1f6291021f0d5a94d77c0c6d0e87a4dc106dcce4
Majid Equbal <majide@neuronimbus.com>
2025-11-18 13:58:15 +0530

    updated new changes as per feedback

--- react_website/src/components/VideoSlider.jsx
+++ react_website/src/components/VideoSlider.jsx
@@ -1,49 +1,34 @@
-import { Container, Image, Row, Col } from "react-bootstrap";
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Image } from "react-bootstrap";
 import { Navigation } from "swiper/modules";
+import arrowLeft from "../assets/nav-arrow-left.svg";
+import arrowRight from "../assets/nav-arrow-right.svg";
 
 import "swiper/css";
 import "swiper/css/navigation";
 
-function VideoSlider() {
+export default function VideoSlider() {
+  const slides = Array.from({ length: 10 }, (_, i) => i + 1);
+  const total = slides.length;
+
+  const [current, setCurrent] = useState(1);
+
   return (
-    <>
-      <section className="sec sec-slider">
-        <Swiper
-          spaceBetween={20}
-          slidesPerView={4.5}
-          navigation
-          modules={[Navigation]}
-        >
-          <SwiperSlide>
-            <div className="artist-video">
-              <a
-                href="#"
-                className="artist-video-text text-center justify-content-center align-items-center"
-              >
-                <Image
-                  src="https://www.shutterstock.com/shutterstock/videos/1108636195/thumb/5.jpg"
-                  alt=""
-                />
-                <span className="artist-video-play">&nbsp;</span>
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="artist-video">
-              <a
-                href="#"
-                className="artist-video-text text-center justify-content-center align-items-center"
-              >
-                <Image
-                  src="https://upload.wikimedia.org/wikipedia/commons/3/36/Indian_village_musicians.jpg"
-                  alt=""
-                />
-                <span className="artist-video-play">&nbsp;</span>
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
+    <section className="sec sec-slider">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={20}
+        slidesPerView={4.5}
+        loop={true}
+        navigation={{
+          prevEl: "#prevBtn",
+          nextEl: "#nextBtn",
+        }}
+        onSlideChange={(swiper) => setCurrent(swiper.activeIndex + 1)}
+      >
+        {slides.map((item) => (
+          <SwiperSlide key={item}>
             <div className="artist-video">
               <a
                 href="#"
@@ -53,40 +38,29 @@ function VideoSlider() {
               </a>
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="artist-video">
-              <a
-                href="#"
-                className="artist-video-text text-center justify-content-center align-items-center"
-              >
-                <span className="artist-video-play">&nbsp;</span>
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="artist-video">
-              <a
-                href="#"
-                className="artist-video-text text-center justify-content-center align-items-center"
-              >
-                <span className="artist-video-play">&nbsp;</span>
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="artist-video">
-              <a
-                href="#"
-                className="artist-video-text text-center justify-content-center align-items-center"
-              >
-                <span className="artist-video-play">&nbsp;</span>
-              </a>
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </section>
-    </>
+        ))}
+      </Swiper>
+
+      <div className="swiper-nav d-flex flex-column align-items-center mt-4">
+        <div className="swiper-nav-fraction">
+          <span className="current fs-5 fw-bolder">{current}</span>
+          <span className="total text-muted"> / {total}</span>
+        </div>
+        <div className="d-inline-flex align-items-center">
+          <button className="nav-btn prev-btn" id="prevBtn">
+            <Image src={arrowLeft} alt="" />
+          </button>
+          <div className="progress-line">
+            <div
+              className="progress-fill"
+              style={{ width: `${(current / total) * 100}%` }}
+            ></div>
+          </div>
+          <button className="nav-btn next-btn" id="nextBtn">
+            <Image src={arrowRight} alt="" />
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
-
-export default VideoSlider;
