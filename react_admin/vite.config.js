0a95970f044c02e66273f5656e0addeec2d0445a
vijaycnn <163419645+vijaycnn@users.noreply.github.com>
2025-11-29 14:16:35 +0530

    Update vite.config.js

--- react_admin/vite.config.js
+++ react_admin/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : '/madmin2025/',
+  base : '/admin/',
   plugins: [react()],
   server: {
     port: 5174, // 👈 change this to your custom port
