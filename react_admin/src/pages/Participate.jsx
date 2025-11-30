10235641e227293b0244aba0e3abc6157bba1345
ajit kumar <50000875@mahindra.com>
2025-11-30 18:13:49 +0530

    add search and download option

--- react_admin/src/pages/Participate.jsx
+++ react_admin/src/pages/Participate.jsx
@@ -1,13 +1,5 @@
-import {
-  Container,
-  Form,
-  Badge,
-  Row,
-  Col,
-  Button,
-  Table,
-} from "react-bootstrap";
-import { BiPencil } from "react-icons/bi";
+import {Container, Form,Badge,Row,Col,Button,Table,} from "react-bootstrap";
+// import { BiPencil } from "react-icons/bi";
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
@@ -16,19 +8,62 @@ import { getDownloadUrl, cityList } from "../api";
 import moment from "moment";
 import axiosInstance from "../helper/constants/axiosInstance";
 const adminAlias = import.meta.env.VITE_API_ADMIN_ALIAS;
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 function Participate() {
   const [offset, setOffset] = useState(0);
   const [perPage, setPerPage] = useState(20);
   const [pageCount, setPageCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [filterKeyword, setFilterKeyword] = useState(null);
   const [totalRecords, setTotalRecords] = useState(0);
   const [items, setItems] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const S3_baseurl = import.meta.env.VITE_API_S3FILE_URL;
+  // const S3_baseurl = import.meta.env.VITE_API_S3FILE_URL;
   const navigate = useNavigate();
 
+  const [filteredData, setFilteredData] = useState({});
+  const [search, setSearch] =  useState({
+    startDate: "",
+    endDate: "",
+    roleType: "",		
+	});  
+  const formatDate = (date) => new Date(date).toISOString().split("T")[0];
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    const today = formatDate(new Date());
+
+    setSearch(prev => {
+      let updated = { ...prev, [name]: value };
+
+      if (name === "startDate") {
+        // If startDate <= today → auto-set endDate to today
+        if (value && value <= today) {
+          updated.endDate = today;
+        }
+      }
+
+      return updated;
+    });
+  };	
+	const searchData = () => {
+		setFilteredData(search);
+		setOffset(0);
+		setCurrentPage(0);
+	}
+
+	const reset = () => {
+    setSearch({
+      startDate: "",
+      endDate: "",
+      roleType: ""
+    });
+    setFilteredData({})
+    setOffset(0);
+		setCurrentPage(0);		
+	}
+
+  const roleList = [ 'Composer', 'Lyricist', 'Music Producer', 'Singer', 'Songwriter', 'Others' ]
   const handlePageClick = (e) => {
     const selectedPage = e.selected;
     let offset = selectedPage * perPage;
@@ -38,7 +73,8 @@ function Participate() {
   const getEnquiryList = async () => {
     setIsLoading(true);
     
-    const body = { params: { offset: offset, perPage: perPage } };
+    const body = { params: { offset: offset, perPage: perPage, ...filteredData } };
+    console.log('body>>> ', body);
     await axiosInstance.get(`/enquiry`, body)
 			.then((response) => {
         console.log('>>> ', response.data);
@@ -57,16 +93,6 @@ function Participate() {
 				setIsLoading(false)
 				// showAlert("Can not get Leads.", "danger")
 			});
-    // let result = await enquiryList(params);
-    // if (result?.status == "success") {
-    //   if (result?.data) {
-    //     // console.log('>>> ', result.data);
-    //     setPageCount(Math.ceil(result?.totalRecords / perPage));
-    //     setItems(result.data);
-    //     setTotalRecords(result.totalRecords);
-    //   }
-    // }
-    // setIsLoading(false);
   };
 
   const handleLogout = () => {
@@ -95,7 +121,59 @@ function Participate() {
     if (!isLoading) {
       getEnquiryList();
     }
-  }, [offset]);
+  }, [offset, perPage, filteredData]);
+
+  const handleExportExcel = async () => {
+    setIsLoading(true);
+      try {
+        const body = { params: { offset: 0, perPage: 0, ...filteredData } };
+        console.log('body>>> ', body);
+        const res = await axiosInstance.get(`/enquiry`, body);
+        if (res.data?.data?.length === 0) {
+          alert("No data found to export!");  setIsLoading(false);
+          return;
+        }
+
+        const exportData = res.data.data.map((item, index) => ({
+          "S.No": index + 1,
+          "Participate ID": item.id ? "HMA"+String(item.id).padStart(5, "0") : "",
+          "Submission Date": item.createdAt ? new Date(item.createdAt).toLocaleDateString() : "",
+          "Name": item.name || "",
+          "Phone": item.contact || "",
+          "Email": item.email || "",
+          "DOB": item.dob ? new Date(item.dob).toLocaleDateString() : "",
+          "Role Interested": item.interest_in_role || "",
+          "Other Role": item.other_roles || "",
+          "Dream": item.dream_remarks || "",
+          "How to know about this": item.how_to_know_about_this || "",
+          "Story": item.story || "",
+          "State": item?.StateMaster?.stateName || "",
+          "City": item?.CityMaster?.cityName || "",
+          "Address": item.address || "",
+          "Pincode": item.pincode || "",
+          "Media File URL": item.media_url || "",
+        }));
+
+        const worksheet = XLSX.utils.json_to_sheet(exportData);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Enquiry Data");
+
+        const excelBuffer = XLSX.write(workbook, {
+          bookType: "xlsx",
+          type: "array",
+        });
+        const fileData = new Blob([excelBuffer], {
+          type: "application/octet-stream",
+        });
+
+        saveAs(fileData, `ParticipateList_${new Date().toISOString().slice(0,10)}.xlsx`);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.log("Export Error:", error);
+      }
+      setIsLoading(false);
+  };
 
   const showItems = () => {
     return isLoading == false ? (
@@ -190,44 +268,45 @@ function Participate() {
   return (
     <>
       <h1 className="h4 mb-4 font-secondary fw-medium">Participant</h1>
-      {/* <h5 className="mb-0">Total Records : {totalRecords}</h5> */}
-
-      {/* <div className="bg-white p-4 rounded mb-4">
+      <div className="bg-white p-4 rounded mb-4">
         <h6 className="font-secondary text-muted fw-medium mb-4">Filters</h6>
         <Row>
           <Col md={4}>
             <Form.Group className="mb-3">
               <Form.Label>Select Role</Form.Label>
-              <Form.Select>
-                <option value="">Select</option>
+              <Form.Select name="roleType" value={search.roleType} onChange={handleFilterChange}>
+                <option key="" value="">Select All</option>
+                {roleList.map((role, $index) =>
+                    <option key={$index+1} value={role}>{role}</option>
+                 )}
               </Form.Select>
             </Form.Group>
           </Col>
           <Col md={4}>
             <Form.Group className="mb-3">
               <Form.Label>Duration - Start Date</Form.Label>
-              <Form.Control type="date" />
+              <Form.Control type="date" value={search.startDate} onChange={handleFilterChange} name="startDate"/>
             </Form.Group>
           </Col>
           <Col md={4}>
             <Form.Group className="mb-3">
               <Form.Label>Duration - End Date</Form.Label>
-              <Form.Control type="date" />
+              <Form.Control type="date" value={search.endDate} onChange={handleFilterChange} minDate={search.startDate} name="endDate"/>
             </Form.Group>
           </Col>
         </Row>
         <div className="d-flex justify-content-center gap-2">
-          <Button variant="primary" size="sm">
+          <Button variant="primary" size="sm" onClick={searchData} >
             <span>Search</span>
           </Button>
-          <Button variant="outline-secondary" size="sm">
+          <Button variant="outline-secondary" size="sm" onClick={reset} > 
             Reset
           </Button>
-          <Button variant="outline-secondary" size="sm">
+          <Button variant="outline-secondary" size="sm" disabled={!items || items.length == 0} onClick={handleExportExcel}>
             Download
           </Button>
         </div>
-      </div> */}
+      </div>
       <div className="table-view bg-white rounded-4 p-4">
         <div className="text-muted mb-3">
           Total Records :{" "}
