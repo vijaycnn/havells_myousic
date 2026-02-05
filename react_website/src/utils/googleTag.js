2f2c5e0ef24facf8488494dc3ef05af72ea0c5fc
ajit kumar <50000875@mahindra.com>
2026-02-05 17:00:08 +0530

    add googleTagScript

---
+++ react_website/src/utils/googleTag.js
@@ -0,0 +1,22 @@
+export const loadGA4 = (gaId) => {
+  if (!gaId) return;
+
+  // Prevent duplicate load
+  if (window.gtag) return;
+
+  // Load gtag.js
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script);
+
+  // Init GA
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  window.gtag = gtag;
+
+  gtag("js", new Date());
+  gtag("config", gaId);
+};
